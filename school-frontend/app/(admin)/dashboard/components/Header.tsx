74d4d2813973daeeb594c78afcab2e9ea29a0abd
amir-mc <amirfooladi93@gmail.com>
2025-12-09 09:02:10 +0000

    add remove token to user side

--- school-frontend/app/(admin)/dashboard/components/Header.tsx
+++ school-frontend/app/(admin)/dashboard/components/Header.tsx
@@ -1,9 +1,18 @@
+import { useRouter } from "next/navigation";
+
+
 // app/(admin)/components/Header.tsx
 export default function Header() {
+  const router=useRouter()
+  const handelClickLogOut = ()=>{
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    router.push('/login')
+  }
   return (
     <header className="h-16 bg-white shadow-sm border-b border-gray-200 flex items-center justify-between px-6">
       <div className="text-lg font-semibold">خوش آمدید ادمین عزیز 👋</div>
-      <button className="text-sm text-red-600 hover:underline">خروج</button>
+      <button onClick={handelClickLogOut} className="text-sm text-red-600 hover:underline">خروج</button>
     </header>
   )
 }
