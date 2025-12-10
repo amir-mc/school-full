8cb131bf4ff8637321f5d6f04babcfeb7b67895d
amir-mc <amirfooladi93@gmail.com>
2025-12-10 10:24:42 +0000

    add test

--- school-frontend/app/(admin)/dashboard/components/Header.tsx
+++ school-frontend/app/(admin)/dashboard/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
   }
   return (
     <header className="h-16 bg-white shadow-sm border-b border-gray-200 flex items-center justify-between px-6">
-      <div className="text-lg font-semibold">خوش آمدید ادمین عزیز 👋</div>
+      <div className="text-lg font-semibold">خوش آمدید ادمین عزیز</div>
       <button onClick={handelClickLogOut} className="text-sm text-red-600 hover:underline">خروج</button>
     </header>
   )
